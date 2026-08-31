e55bca5f935021b8fce367e88c8dce3508954c5a
Henrique Farias Vacheleski <henrique_vacheleski@estudante.sesisenai.org.br>
2026-08-31 12:12:19 -0300

    Segundo Commit

---
+++ 02_operadores_lógicos_estruturas_condicionais/Exemplo.py
@@ -0,0 +1,45 @@
+# Aula: OPERADORES LÓGICOS E ESTRUTURAS CONDICIONAIS
+# 1. OPERADORES LÓGICOS
+
+# and
+# Todas as condições precisam ser verdadeiras
+
+idade = 20
+possui_carteira = True
+
+resultado = idade >= 18 and possui_carteira
+print(resultado)
+
+# or
+# Pelo menos uma das condições precisa ser verdadeira
+
+idade = 16
+acompanhado = True
+
+resultado = idade >= 18 or acompanhado
+print(resultado)
+
+#not
+# Inverte o resultado de uma condição
+
+Aluno_matriculado = True
+
+print(not Aluno_matriculado)
+
+# 2. OPERADORES DE COMPARAÇÃO
+
+idade = 18
+
+print(idade == 18)
+print(idade != 18)
+print(idade > 18)
+print(idade < 18)
+print(idade >= 18)
+print(idade <= 18)
+
+# 3. ESTRUTURA if
+
+idade = 18
+
+if idade >= 18:
+    print("Maior de idade")
